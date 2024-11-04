aa87e5dc860b3e5951cfe176e89663e791905e49
campique <info@campique.com>
2024-11-04 21:56:14 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('draw', (data) => {
+    socket.broadcast.emit('draw', data);
+  });
+
+  socket.on('clear', () => {
+    socket.broadcast.emit('clear');
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
